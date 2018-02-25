320c019ebc8b4ebada5cafb2b9c2885727728362
mgriswold19 <matthewgriswold@dhcp-10-105-128-83.wireless.northwestern.private>
2018-02-25 12:39:10 -0600

    new loop

--- scraper.py
+++ scraper.py
@@ -4,19 +4,36 @@ import urllib.request
 import unicodedata
 import datetime
 
-ar_url = "https://wikileaks.org/clinton-emails/emailid/1149"
+badcount = 0
+emails = []
+##Current Schema: List of lists. Each email: [from,to,date]
+##Better data scrctures? does a class make sense here??
 
-with urllib.request.urlopen(ar_url) as url:
-	ar_html = url.read()
-	ar_soup = BeautifulSoup(ar_html, 'html.parser')
-	header = ar_soup.find("header").contents
-	emailfrom = BeautifulSoup(str(header[1]), 'html.parser').find("span").contents[0]
-	emailto = BeautifulSoup(str(header[3]), 'html.parser').find("span").contents[0]
-	emaildatestring = header[4].split('\n\t\t\t\t\t')[1][6:]
-	emaildate = datetime.datetime.strptime(emaildatestring[:10], '%Y-%m-%d').date()
-	print(header)
-	print("from:")
-	print(emailfrom)
-	print("to:")
-	print(emailto)
-	print(emaildate)
+
+#for i in range(33727):
+for i in range(500):
+
+	ar_url = "https://wikileaks.org/clinton-emails/emailid/" + str(i)
+
+	with urllib.request.urlopen(ar_url) as url:
+		ar_html = url.read()
+		ar_soup = BeautifulSoup(ar_html, 'html.parser')
+		try:
+			header = ar_soup.find("header").contents
+			emailfrom = BeautifulSoup(str(header[1]), 'html.parser').find("span").contents[0]
+			emailto = BeautifulSoup(str(header[3]), 'html.parser').find("span").contents[0]
+			emaildatestring = header[4].split('\n\t\t\t\t\t')[1][6:]
+			emaildate = datetime.datetime.strptime(emaildatestring[:10], '%Y-%m-%d').date()
+			emails.append([emailfrom,emailto,emaildate])
+		except:
+			badcount += 1
+		# print(header)
+		# print("from:")
+		# print(emailfrom)
+		# print("to:")
+		# print(emailto)
+		# print(emaildate)
+print("writing")
+with open(wikileaksout.csv, 'wb') as myfile:
+    wr = csv.writer(myfile)
+    wr.writerow(emails)
