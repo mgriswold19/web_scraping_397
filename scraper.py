59333c6068304d11ca2a4dfcae4919eb15d9f298
mgriswold19 <matthewgriswold@Matthews-MacBook-Pro-2.local>
2018-02-25 14:06:11 -0600

    lots of new fun stuff

--- scraper.py
+++ scraper.py
@@ -4,9 +4,11 @@ import urllib.request
 import unicodedata
 import datetime
 import csv
+import pandas as pd
 
 badcount = 0
 emails = []
+df = pd.DataFrame(columns=["emaildate","otherhuman"])
 ##Current Schema: List of lists. Each email: [from,to,date]
 ##Better data scrctures? does a class make sense here??
 
@@ -26,6 +28,13 @@ for i in range(10):
 			emaildatestring = header[4].split('\n\t\t\t\t\t')[1][6:]
 			emaildate = datetime.datetime.strptime(emaildatestring[:10], '%Y-%m-%d').date()
 			emails.append([emailfrom,emailto,emaildate])
+			human = ""
+			if emailfrom == "Hillary Clinton":
+				human = emailto
+			else:
+				human = emailfrom
+			#df = df.append([emaildate,human]);
+			df.loc[len(df.index)] = [emaildate,human]
 		except:
 			badcount += 1
 		# print(header)
@@ -34,8 +43,27 @@ for i in range(10):
 		# print("to:")
 		# print(emailto)
 		# print(emaildate)
-print("writing")
+
+#setting the date as the index
+df['dateindex'] = pd.to_datetime(df['emaildate'])
+df = df.set_index('dateindex')
+df = df.drop('emaildate', 1)
+
+#getting the frequency 
+
+dfvalcount = df.loc["2010-06-24":"2010-06-27"].otherhuman.value_counts()
+
+print("list:")
 print(emails)
-with open("wikileaksout.csv", 'w') as myfile:
-    wr = csv.writer(myfile)
-    wr.writerow(emails)
+print("dataframe:")
+print(df)
+print("value count:")
+print(dfvalcount)
+
+
+#write to csv
+
+dfvalcount.to_csv("wikivalout.csv")
+# with open("wikileaksout.csv", 'w') as myfile:
+#     wr = csv.writer(myfile)
+#     wr.writerow(emails)
